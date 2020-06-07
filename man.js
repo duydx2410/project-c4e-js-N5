473c17e7f7b0f9d15b083c29db65ee9177ad30db
duy <duydx2410@gmail.com>
2020-06-07 16:11:14 +0700

    update

--- man.js
+++ man.js
@@ -1,7 +1,7 @@
 let listDataProduct = [
     {
         name: 'Áo nữ HeraGD tay ngắn màu hồng',
-        price: 285e3 ,
+        price: '285.000' ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/16065759084574-600x866.jpg',
         description: `<ul>
                         <li>Chất liệu vải lụa nhuyễn</li>
@@ -13,7 +13,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo kiểu H:CONNECT cổ trụ tay lỡ bo thun màu xanh ngọc',
-        price: 299e3 ,
+        price: '299.000' ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/16315887058974-600x866.jpg',
         description: `<ul>
                         <li>Chất liệu vải lụa nhuyễn</li>
@@ -25,7 +25,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo kiểu tay lỡ The One Fashion màu đen',
-        price: 168e3 ,
+        price: '168.000' ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/13067119853598-600x866.jpg',
         description: `<ul>
                         <li>Chất liệu vải lụa nhuyễn</li>
@@ -37,7 +37,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo kiểu H:CONNECT cổ thắt dây nơ họa tiết hoa',
-        price: 299e3 ,
+        price: '299.000' ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/16314939670558-600x866.jpg',
         description: `<ul>
                         <li>Chất liệu vải lụa nhuyễn</li>
@@ -49,7 +49,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo chống nắng nam',
-        price: 399e3 ,
+        price: '349.000' ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/o/8ot20s001-sb128-l-.jpg',
         description: `<ul>
                         <li>Chất liệu polyester co giãn</li>
@@ -61,7 +61,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo sơ mi nam ngắn tay',
-        price: 349e3 ,
+        price: '349.000' ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/t/8th20c001-pb208-l.jpg',
         description: `<ul>
                         <li>Chất liệu 50% bamboo 50% polyester.</li>
@@ -73,7 +73,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo sơ mi ngắn tay trơn',
-        price: 499e3 ,
+        price: '499.000' ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/t/8th20s018-se003-m.jpg',
         description: `<ul>
                         <li>Chất liệu 50% bamboo 50% polyester.</li>
@@ -85,7 +85,7 @@ let listDataProduct = [
     },
     {
         name: 'Áo sơ mi ngắn tay kẻ caro',
-        price: 349e3 ,
+        price: '349.000' ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/t/8th20a004-cg068-m.jpg',
         description: `<ul>
                         <li>Chất liệu 50% bamboo 50% polyester.</li>
@@ -97,3 +97,26 @@ let listDataProduct = [
     }, 
 ];
 
+let tbody = document.getElementById('product-group-man');
+    tbody.innerHTML = '';
+
+    for (let i = 0; i < listDataProduct.length; i++) {
+        if(listDataProduct[i].gender === "Thời trang Nam") {
+            let product = listDataProduct[i];
+        
+        let html = `<tr>
+                       
+                        <td>
+                            <img src="${product.img}" alt="" width="150" height="200">
+                        </td>
+                        <td>${product.name}</td>
+                        <td>${product.description}</td>
+                        <td>${product.price}đ</td>
+                        <td>
+                            <button class="btn btn-link" onclick="showDetail(${product.id})">Detail</button>
+                        </td>
+                    </tr>`;
+        
+        tbody.innerHTML += html;
+        }
+    }
--- woman.js
+++ woman.js
@@ -1,5 +1,6 @@
 let listDataProduct = [
     {
+        id: 1,
         name: 'Áo nữ HeraGD tay ngắn màu hồng',
         price: 285e3 ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/16065759084574-600x866.jpg',
@@ -12,6 +13,7 @@ let listDataProduct = [
         gender: "Thời trang Nữ",
     },
     {
+        id: 2,
         name: 'Áo kiểu H:CONNECT cổ trụ tay lỡ bo thun màu xanh ngọc',
         price: 299e3 ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/16315887058974-600x866.jpg',
@@ -24,6 +26,7 @@ let listDataProduct = [
         gender: "Thời trang Nữ",
     },
     {
+        id:3,
         name: 'Áo kiểu tay lỡ The One Fashion màu đen',
         price: 168e3 ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/13067119853598-600x866.jpg',
@@ -35,7 +38,8 @@ let listDataProduct = [
         type: "Áo",
         gender: "Thời trang Nữ",
     },
-    {
+    { 
+        id: 4,
         name: 'Áo kiểu H:CONNECT cổ thắt dây nơ họa tiết hoa',
         price: 299e3 ,
         img: 'https://salegiagoc.com/wp-content/uploads/2018/12/16314939670558-600x866.jpg',
@@ -48,6 +52,7 @@ let listDataProduct = [
         gender: "Thời trang Nữ",
     },
     {
+        id: 5,
         name: 'Áo chống nắng nam',
         price: 399e3 ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/o/8ot20s001-sb128-l-.jpg',
@@ -60,6 +65,7 @@ let listDataProduct = [
         gender: "Thời trang Nam",
     },
     {
+        id: 6,
         name: 'Áo sơ mi nam ngắn tay',
         price: 349e3 ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/t/8th20c001-pb208-l.jpg',
@@ -72,6 +78,7 @@ let listDataProduct = [
         gender: "Thời trang Nam",
     },
     {
+        id: 7,
         name: 'Áo sơ mi ngắn tay trơn',
         price: 499e3 ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/t/8th20s018-se003-m.jpg',
@@ -84,6 +91,7 @@ let listDataProduct = [
         gender: "Thời trang Nam",
     },
     {
+        id: 8,
         name: 'Áo sơ mi ngắn tay kẻ caro',
         price: 349e3 ,
         img: 'https://canifa.s3.amazonaws.com/media/catalog/product/8/t/8th20a004-cg068-m.jpg',
@@ -96,4 +104,54 @@ let listDataProduct = [
         gender: "Thời trang Nam",
     }, 
 ];
+function showProducts(listDataProduct){
+    let tbody = document.getElementById('product-group-woman');
+    tbody.innerHTML = '';
+
+    for (let i = 0; i < listDataProduct.length; i++) {
+        if(listDataProduct[i].gender === "Thời trang Nữ") {
+            const product = listDataProduct[i];
+        
+        let html = `<tr>
+                       
+                        <td>
+                            <img src="${product.img}" alt="" width="150" height="200">
+                        </td>
+                        <td>${product.name}</td>
+                        <td>${product.description}</td>
+                        <td>${product.price}đ</td>
+                        <td>
+                            <button class="btn btn-link" onclick="showDetail(${product.id})">Detail</button>
+                        </td>
+                    </tr>`;
+        
+        tbody.innerHTML += html;
+        }
+    }
+}
+    showProducts(listDataProduct);
+
+    function showDetail(id){
+        //1: save productId selected
+        localStorage.setItem('selectedProductId', id);
+        localStorage.setItem('products', JSON.stringify(listDataProduct));
+        //2: redirect
+        window.location.href = 'detail.html';
+    }
+    
+    // function search(){
+    //     let sName = document.getElementById('search-product-name').value;
+    //     let priceMin = parseInt(document.getElementById('search-product-price-min').value);
+    //     let priceMax = parseInt(document.getElementById('search-product-price-max').value);
+    //     let result = [];
+    //     result = products.filter(function (value){
+    //         return (sName.trim() !== '' && value.name.toLowerCase().includes(sName.toLowerCase()))
+    //         &&
+    //         (!isNaN(priceMin) && value.price >= priceMin
+    //             &&
+    //         !isNaN(priceMax) && value.price <= priceMax);
+    //     });
+    
+    //     showProducts(result);
+    // }
 
